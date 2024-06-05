8d46e6d792f5ef831ed5995e5ae0f980fd978b2a
Jaydip Umaretiya <jaydip@enlistech.com>
2024-06-05 22:43:07 +0530

    Updates libraries

--- core/core-dependencies/build.gradle.kts
+++ core/core-dependencies/build.gradle.kts
@@ -52,35 +52,15 @@ dependencies {
     api("com.google.code.gson:gson:2.10.1")
 
     // Room components
-    api("androidx.room:room-runtime:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
-    api("androidx.room:room-coroutines:2.1.0-alpha04")
+    api(libs.bundles.room)
+    ksp(libs.room.compile)
 
-    //NetWork
-    api("com.squareup.retrofit2:retrofit:2.9.0")
-    api("com.squareup.retrofit2:converter-moshi:2.9.0")
-    api("com.squareup.retrofit2:converter-gson:2.9.0")
-    api("com.squareup.retrofit2:converter-scalars:2.9.0")
-    // http logger
-    api("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.5")
-    api("com.squareup.okhttp3:okhttp:5.0.0-alpha.5")
-
-    // Dependency Injection
-    // implementation 'io.insert-koin:koin-core:3.0.1'
-    api("io.insert-koin:koin-android:2.0.1")
-    api("io.insert-koin:koin-androidx-scope:2.0.1")
-    api("io.insert-koin:koin-androidx-viewmodel:2.0.1")
-
-    //Coroutine
+    // Coroutine
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
 
-    //Arch Components
-//    implementation 'androidx.lifecycle:lifecycle-runtime:2.5.1'
-//    implementation 'androidx.lifecycle:lifecycle-common:2.5.1'
-//    implementation 'androidx.lifecycle:lifecycle-livedata-ktx:2.5.1'
-//    implementation 'androidx.lifecycle:lifecycle-extensions:2.2.0'
-
     // Glide - https://github.com/bumptech/glide
     api("com.github.bumptech.glide:glide:4.14.2")
+
+    api(libs.bundles.retrofit)
 }
